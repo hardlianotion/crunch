2bace81c2951672b62ccd58ba520058c49f11488
Etuka Onono <etuka@urbantz.com>
2022-01-01 23:22:37 +0100

    Bugfixes for new situations encountered in statement update.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,7 @@
 package crunch
 
 import java.time.format.DateTimeFormatter
-import java.time.LocalDate
+import java.time.{LocalDate, Duration}
 import java.io.{FileWriter, BufferedWriter, File}
 
 import scala.annotation.tailrec
@@ -49,18 +49,23 @@ object CurrencyMappedStatement:
     eur2gbp: Double
   )
 
+  case class FxEntry (
+    at: LocalDate,
+    eur2gbp: Double
+  )
+
   def line2Entry (line: String): BaseEntry = 
-    val Array (date, counter, ref, kind, amount, balance, _) = 
-      line.split (",").map (_.trim)
+    val Array (date, counter, ref, kind, amount, balance, _) =
+      line.split (",")//.map (_.trim)
     val formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu")
     
     BaseEntry (LocalDate.parse (date, formatter), counter, ref, kind, amount.toDouble, balance.toDouble)
       
 
-  def line2Fx (line: String): Fx = 
-    val Array (at, fixedAt, eur2gbp) = 
+  def line2Fx (line: String): FxEntry =
+    val Array (at, eur2gbp) =
       line.split (",").map (_.trim)
-    Fx (LocalDate.parse (at), LocalDate.parse (fixedAt), eur2gbp.toDouble)
+    FxEntry (LocalDate.parse (at), eur2gbp.toDouble)
 
   def eurHeader: String = 
     "Date,Counter Party,Reference,Type,Amount (EUR),Balance (EUR),Spending Category,Notes"
@@ -68,12 +73,12 @@ object CurrencyMappedStatement:
   def gbpHeader: String = 
     "Date,Counter Party,Reference,Type,Amount (GBP),Balance (GBP),Spending Category,Notes"
   
-  def readEntries (path: String): Try [IndexedSeq [Entry]] =
+  def readEntries (reconcileWith: String, path: String): Try [IndexedSeq [Entry]] =
     @tailrec
     def impl (curr: BaseEntry, input: Iterator [String], output: IndexedSeq [Entry]): IndexedSeq [Entry] =
       if input.hasNext then
         val next = line2Entry (input.next)
-        if curr.kind == "CURRENCY TRANSFER" then
+        if curr.kind == "CURRENCY TRANSFER" && curr.counter == reconcileWith then
           val transfer = TransferEntry (DateRank (curr.date, 0), curr.counter, curr.reference, curr.amount, curr.balance)
           
           impl (next, input, output :+ transfer)
@@ -89,14 +94,21 @@ object CurrencyMappedStatement:
       impl (be, lines, IndexedSeq.empty [Entry])     
     }
   
-  def readFx (path: String): Try [Iterator [Fx]] =
+  def readFx (path: String): Try [Iterator [Option [FxEntry]]] =
     Try {
       val source = fromFile (path)
       for
         line <- source.getLines.drop (1)
       yield
-        line2Fx (line)
+        Try {line2Fx (line)}.toOption
     }
+
+  def buildFxMap (entries: Iterator [Option [FxEntry]]): SortedMap [LocalDate, Double] =
+    entries.foldLeft (List.empty [FxEntry]) { (agg, rhs) => rhs match
+      case Some (entry) => entry :: agg
+      case None => agg
+    }.map (x => (x.at.plusDays (1) -> x.eur2gbp)).to [SortedMap [LocalDate, Double]] (SortedMap)
+
 //  val file = File(s"data/service/service-time-gap-${ServiceTime.minServiceGap}-arr-${ServiceTime.arrivalSearchLimit}-pro-${ServiceTime.proximityLimit}-spd-${ServiceTime.loSpeedLimit}-dil-${ServiceTime.timeDilator}-ovr-${ServiceTime.acceptOverlap}.csv ")
 //      val baselineOut = BufferedWriter (FileWriter (file))
 
@@ -105,9 +117,9 @@ object CurrencyMappedStatement:
     val balStr = f"${entry.balance}%2.2f"
     writer.write (s"${entry.date},${entry.counter},${entry.reference},${entry.kind},${amountStr},${balStr}\n")
 
-  def runAccounts (fxPath: String, eurPath: String, gbpPath: String) =
-    val maybeEurEntries = readEntries (eurPath)
-    val maybeGbpEntries = readEntries (gbpPath)
+  def runAccounts (reconcileWith: String, fxPath: String, eurPath: String, gbpPath: String) =
+    val maybeEurEntries = readEntries (reconcileWith, eurPath)
+    val maybeGbpEntries = readEntries (reconcileWith, gbpPath)
     val maybeFxEntries = readFx (fxPath)
     val file = File ("eur2gbpout.csv")
     val output = BufferedWriter (FileWriter (file))
@@ -118,7 +130,7 @@ object CurrencyMappedStatement:
       gbpEntries <- maybeGbpEntries
       eurEntries <- maybeEurEntries
     do
-      val fxMap = fxEntries.map (x => (x.fixedAt -> x.eur2gbp)).to [SortedMap [LocalDate, Double]] (SortedMap)
+      val fxMap = buildFxMap (fxEntries)
       val gbpTransfers = 
         gbpEntries
           .foldLeft (Map.empty [DateRank, TransferEntry]) { 
@@ -138,7 +150,7 @@ object CurrencyMappedStatement:
         eurEntry <- eurEntries
       do
         eurEntry match 
-          case te @ TransferEntry (dt, counter, ref, amt, bal) => 
+          case te @ TransferEntry (dt, counter, ref, amt, bal) =>
             val gbpTran = gbpTransfers (dt)
             val amount = -gbpTran.amount
             writeEntry (BaseEntry (dt.at, counter, ref, "TRANSFER CURRENCY", amount, bal + amount), output)           
@@ -151,7 +163,8 @@ object CurrencyMappedStatement:
   @main
   def run =
     runAccounts (
-      "data/Reconciliation FX Account - FX EUR-GB.csv", 
-      "data/StarlingStatement_2021-03-01_2021-11-20-eur.csv", 
-      "data/StarlingStatement_2021-03-01_2021-11-20-gbp.csv")
+      "Ergates Limited",
+      "data/fx-eurgbp.csv",
+      "data/StarlingStatement_2021-01-15_2021-12-29-eur.csv",
+      "data/StarlingStatement_2021-01-01_2021-12-29-gbp.csv")
 
